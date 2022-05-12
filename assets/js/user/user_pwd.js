d5ffe377abf1c4aec9e8aae98f7bd28ebdfd8245
itheima <itheima@itcast.cn>
2022-05-13 01:04:31 +0800

    完成个人中心功能的开发

--- assets/js/user/user_pwd.js
+++ assets/js/user/user_pwd.js
@@ -1,34 +1,43 @@
 $(function() {
-  var form = layui.form
+    var form = layui.form
 
-  form.verify({
-    pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
-    samePwd: function(value) {
-      if (value === $('[name=oldPwd]').val()) {
-        return '新旧密码不能相同！'
-      }
-    },
-    rePwd: function(value) {
-      if (value !== $('[name=newPwd]').val()) {
-        return '两次密码不一致！'
-      }
-    }
-  })
-
-  $('.layui-form').on('submit', function(e) {
-    e.preventDefault()
-    $.ajax({
-      method: 'POST',
-      url: '/my/updatepwd',
-      data: $(this).serialize(),
-      success: function(res) {
-        if (res.status !== 0) {
-          return layui.layer.msg('更新密码失败！')
+    // 验证规则
+    form.verify({
+        // 原密码
+        pwd: [/^[\S]{6,12}$/, '密码必须6到12位,且不能出现空格'],
+        // 新密码
+        samePwd: function(value) { //value就是新密码输入的值
+            if (value === $('[name=oldPwd]').val()) {
+                return '新旧密码不能相同！'
+            }
+        },
+        // 确认新密码
+        rePwd: function(value) { //value 确认密码框里输入的值
+            if (value !== $('[name=newPwd]').val()) {
+                return '两次密码不一致！'
+            }
         }
-        layui.layer.msg('更新密码成功！')
-        // 重置表单
-        $('.layui-form')[0].reset()
-      }
     })
-  })
+
+
+    // 发起ajax请求,绑定提交事件--修改密码 .layui-form表单
+    $('.layui-form').on('submit', function(e) {
+        e.preventDefault()
+        $.ajax({
+            method: 'POST',
+            url: '/my/updatepwd',
+            data: $(this).serialize(),
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layui.layer.msg('更新密码失败！')
+                }
+                layui.layer.msg('更新密码成功！')
+                    // 重置表单
+                    //  $('.layui-form') 是jquery元素
+                    // $('.layui-form')[0] 是原生DOM元素
+                    // 需要把jquery元素转换成DOM元素才能调用这个方法
+                $('.layui-form')[0].reset()
+            }
+        })
+    })
 })
